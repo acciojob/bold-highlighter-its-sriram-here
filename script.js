d2570c2b161e00b8d7a21622abeb236f31a04592
SRIRAM <srirammurugesan1807@gmail.com>
2025-07-22 19:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	function highlight() {
+  const strongs = document.querySelectorAll('strong');
+  strongs.forEach(el => {
+    el.style.color = 'rgb(0, 128, 0)';
+  });
+}
+
+function return_normal() {
+  const strongs = document.querySelectorAll('strong');
+  strongs.forEach(el => {
+    el.style.color = 'rgb(0, 0, 0)';
+  });
+}
+
 
     
 }
