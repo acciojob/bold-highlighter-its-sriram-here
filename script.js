e6602d0f2b3024ba31003c2ad98a479f069373ba
SRIRAM <srirammurugesan1807@gmail.com>
2025-07-26 12:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 function highlight() {
-    //Write your code here
-cosnt boldwords = document.querySelectorall('strong');
-	boldWords.forEach(word => {
-		word.style.color = 'rgb(0,128,0)';
-	});
-
-}
-
-function return_normal() {
-  const strongs = document.querySelectorAll('strong');
-  strongs.forEach(el => {
-    el.style.color = 'rgb(0, 0, 0)';
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 128, 0)';
   });
 }
 
-
-    
+function return_normal() {
+  const boldWords = document.querySelectorAll('strong');
+  boldWords.forEach(word => {
+    word.style.color = 'rgb(0, 0, 0)';
+  });
 }
